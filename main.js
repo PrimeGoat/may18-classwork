b58e29a7d5cdf1d4ac9c50da2afd2f3b25cceebd
PrimeGoat <60146830+PrimeGoat@users.noreply.github.com>
2020-05-26 09:12:02 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,42 @@
+//loop through the object using forEach so that it logs, (eg,for user1)
+//'user1 is called Jon and is 23 years old'
+//the loop should not return a data structure
+const obj = {
+user1: {
+name: 'Jon',
+age: 23,
+},
+user2: {
+name: 'Jen',
+age: 25,
+},
+user3: {
+name: 'Jan',
+age: 21,
+},
+}
+// I want a list of all of the documents in the folder, not the files
+// I want one list of all of the files in one data structure
+const folder = {
+taxes: ['2010.xlx', '2020.xlx'],
+records: ['rec1.docx', 'rec2.docx', 'rec3.docx'],
+miscellaneous: ['pic.jpg', 'passwords.pdf', 'docs.txt'],
+Clubhouse: ['paty.jpg', 'christmasRules.pdf', 'pool.txt'],
+}
+///Are the two books alike? Use Object methods to find out. Remember to Change value in one obj to test for non-alike objects
+const book1 = {
+title: 'My Book',
+author: 'John Doe',
+year: '2019',
+}
+const book2 = {
+title: 'My Book',
+author: 'John Doe',
+year: '2019',
+}
+///Double the price of each item in the prices object and place those values in a new object called priceDoubler
+let prices = {
+cigarettes: 5.99,
+coke: { lg: 2.99, sm: 1.99 },
+chips: 2.79,
+}
